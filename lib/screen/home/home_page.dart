bf1a218b7f88ce771b839896e1b646766a2fbbcb
Azhar <azharuddin.koya@kotak.com>
2021-06-10 08:59:02 +0530

    Add Reminder with Dialog

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -5,18 +5,7 @@ import 'package:reminder_app/screen/home/widget/home_list.dart';
 class HomePage extends StatelessWidget {
   HomePage({Key key}) : super(key: key);
 
-  final List<Map<String, dynamic>> _taskList = [
-    {
-      'title': 'Home work',
-      'content': 'Do your home work',
-      'price': 250,
-    },
-    {
-      'title': 'Medication',
-      'content': 'Take Tab before your sleep',
-      'price': 350,
-    },
-  ];
+  final List<Map<String, dynamic>> _taskList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +13,7 @@ class HomePage extends StatelessWidget {
       body: _taskList.isNotEmpty ? HomeList(items: _taskList) : HomeEmptyBody(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xFFFFB51C),
-        onPressed: _fabCLick,
+        onPressed: () => _fabCLick(context),
         child: Icon(
           Icons.add,
           color: Colors.black,
@@ -34,7 +23,82 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  void _fabCLick() {
-    print('Fab Clicked');
+  final TextEditingController _titleController =  TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+
+  void _fabCLick(BuildContext context) {
+    // Navigator.of(context).push(MaterialPageRoute(builder: (con) {
+    //   return Container();
+    // }));
+
+    showDialog(
+      context: context,
+      builder: (_) {
+        final edgeInset = EdgeInsets.symmetric(
+          horizontal: 24.0,
+          vertical: 16.0,
+        );
+
+
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Card(
+              margin: edgeInset,
+              child: Container(
+                padding: edgeInset,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: _descriptionController,
+                      decoration: InputDecoration(labelText: 'Description'),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: _priceController,
+                      decoration: InputDecoration(labelText: 'Price'),
+                    ),
+                    SizedBox(height: 32.0),
+                    ElevatedButton(
+                      onPressed: ()=>_addReminder(context),
+                      child: Text('ADD'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addReminder(BuildContext context) {
+
+
+    String _title = _titleController.text;
+    String _description = _descriptionController.text;
+    double _price = double.parse(_priceController.text);
+
+    // Map<String, dynamic> _map = Map();
+    // _map['title'] = _title;
+    // _map['content'] = _description;
+    // _map['price'] = _price;
+
+    Map<String, dynamic> _map = {
+      'title':_title,
+      'content':_description,
+      'price': _price
+    };
+
+    _taskList.add(_map);
+
+    Navigator.of(context).pop();
   }
 }
