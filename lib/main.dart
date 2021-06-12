752ffafcc4587f24c28b6468e690bad5011b14a4
Azhar <azharuddin.koya@kotak.com>
2021-06-12 13:03:07 +0530

    shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:reminder_app/screen/widgets/utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screen/home/home_page.dart';
 
 void main() {
@@ -12,8 +14,105 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: FutureBuilder(
+        future: userCheck(),
+        builder: (context, snapshots) {
+          print('${snapshots.connectionState}: ${snapshots.data}');
+          if (snapshots.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (snapshots.data != null && snapshots.data) {
+            return HomePage();
+          }
+          return LoginPage();
+        },
+      ),
     );
   }
 
+  Future<bool> userCheck() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await Future.delayed(
+      Duration(seconds: 2),
+    );
+    return prefs.getBool('userExist') ?? false;
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              obscureText: false,
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () => _login(context),
+              child: Text('Login'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _login(BuildContext context) async {
+    String _username = _usernameController.text.trim();
+    String _password = _passwordController.text.trim();
+
+    List<Map<String, dynamic>> users = [
+      {'name': 'azhar', 'password': '12345678'},
+      {'name': 'tariq', 'password': 'abcdefgh'},
+    ];
+
+    if (_username.isEmpty) {
+      toast('Username cannot be empty!');
+    } else if (_password.isEmpty) {
+      toast('Password cannot be empty!');
+    } else if (_password.length < 8) {
+      toast('Password should be min 8 character');
+    } else {
+      bool loginStatus = false;
+      for (int i = 0; i < users.length; i++) {
+        Map<String, dynamic> user = users[i];
+        if (_username == user['name']) {
+          if (_password == user['password']) {
+            loginStatus = true;
+            break;
+          }
+        }
+      }
+
+      if (!loginStatus) {
+        toast('Invalid credentials');
+      } else {
+        //key,value
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setBool('userExist', loginStatus);
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => HomePage(),
+          ),
+        );
+      }
+    }
+  }
 }
--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:reminder_app/screen/home/widget/home_empty.dart';
 import 'package:reminder_app/screen/home/widget/home_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<Map<String, dynamic>> _taskList = [];
 
   @override
@@ -23,23 +28,27 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  final TextEditingController _titleController =  TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+
   final TextEditingController _descriptionController = TextEditingController();
+
   final TextEditingController _priceController = TextEditingController();
 
-  void _fabCLick(BuildContext context) {
+  void _fabCLick(BuildContext context) async {
     // Navigator.of(context).push(MaterialPageRoute(builder: (con) {
     //   return Container();
     // }));
+    print('Before Dialog'); //1
 
-    showDialog(
+    await showDialog(
+      barrierDismissible:false,
       context: context,
       builder: (_) {
+        print('Dialog'); //3
         final edgeInset = EdgeInsets.symmetric(
           horizontal: 24.0,
           vertical: 16.0,
         );
-
 
         return Column(
           mainAxisSize: MainAxisSize.min,
@@ -66,7 +75,7 @@ class HomePage extends StatelessWidget {
                     ),
                     SizedBox(height: 32.0),
                     ElevatedButton(
-                      onPressed: ()=>_addReminder(context),
+                      onPressed: () => _addReminder(context),
                       child: Text('ADD'),
                     ),
                   ],
@@ -77,14 +86,17 @@ class HomePage extends StatelessWidget {
         );
       },
     );
+
+    print('After Dialog'); //2
+    setState(() {});
   }
 
   void _addReminder(BuildContext context) {
-
-
     String _title = _titleController.text;
     String _description = _descriptionController.text;
     double _price = double.parse(_priceController.text);
+
+    _clearController();
 
     // Map<String, dynamic> _map = Map();
     // _map['title'] = _title;
@@ -92,13 +104,19 @@ class HomePage extends StatelessWidget {
     // _map['price'] = _price;
 
     Map<String, dynamic> _map = {
-      'title':_title,
-      'content':_description,
+      'title': _title,
+      'content': _description,
       'price': _price
     };
 
     _taskList.add(_map);
 
     Navigator.of(context).pop();
+  }
+
+  _clearController() {
+    _titleController.clear();
+    _descriptionController.clear();
+    _priceController.clear();
   }
 }
